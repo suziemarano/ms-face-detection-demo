564539aa1847d4774c3bddaedf88e734ae71c2af
Suzie.Marano <Suzie.Marano@SL-WL-275>
2018-02-19 16:21:57 -0800

    Added full demo

---
+++ CognitiveServicesSimpleDemo/FacesPrinter.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Linq;
+using Microsoft.ProjectOxford.Face.Contract;
+
+namespace CognitiveServicesSimpleDemo
+{
+    public static class FacesPrinter
+    {
+        public static void Print(Face[] faces)
+        {
+            foreach (var face in faces)
+            {
+                Console.WriteLine();
+                Console.WriteLine($"Age: {face.FaceAttributes.Age}");
+                Console.WriteLine($"Gender: {face.FaceAttributes.Gender}");
+                Console.WriteLine($"Bald: {face.FaceAttributes.Hair.Bald}");
+                Console.WriteLine($"Accessories: ");
+                if (face.FaceAttributes.Accessories.Any())
+                {
+                    foreach (var accessory in face.FaceAttributes.Accessories)
+                    {
+                        Console.WriteLine($"   {accessory.Type} : {accessory.Confidence}");
+                    }
+                }
+                else
+                {
+                    Console.WriteLine($"   none");
+                }
+                Console.WriteLine($"Hair color:");
+                if (face.FaceAttributes.Hair.HairColor.Any())
+                {
+                    foreach (var hairColor in face.FaceAttributes.Hair.HairColor)
+                    {
+                        Console.WriteLine($"   {hairColor.Color} : {hairColor.Confidence}");
+                    }
+                }
+                else
+                {
+                    Console.WriteLine($"   none");
+                }
+                Console.WriteLine($"Facial Hair: ");
+                Console.WriteLine($"   Beard: {face.FaceAttributes.FacialHair.Beard}");
+                Console.WriteLine($"   Moustache: {face.FaceAttributes.FacialHair.Moustache}");
+                Console.WriteLine($"   Sideburns: {face.FaceAttributes.FacialHair.Sideburns}");
+                Console.WriteLine($"Emotion : ");
+                Console.WriteLine($"   Happy: {face.FaceAttributes.Emotion.Happiness}");
+                Console.WriteLine($"   Neutral: {face.FaceAttributes.Emotion.Neutral}");
+                Console.WriteLine($"   Sad: {face.FaceAttributes.Emotion.Sadness}");
+                Console.WriteLine($"   Anger: {face.FaceAttributes.Emotion.Anger}");
+                Console.WriteLine($"   Contempt: {face.FaceAttributes.Emotion.Contempt}");
+                Console.WriteLine($"   Disgust: {face.FaceAttributes.Emotion.Disgust}");
+                Console.WriteLine($"   Fear: {face.FaceAttributes.Emotion.Fear}");
+                Console.WriteLine($"   Surprise: {face.FaceAttributes.Emotion.Surprise}");
+                Console.WriteLine();
+            }
+        }
+    }
+}
---
+++ CognitiveServicesSimpleDemo/Program.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Threading.Tasks;
+using Microsoft.ProjectOxford.Face;
+using Microsoft.ProjectOxford.Face.Contract;
+
+namespace CognitiveServicesSimpleDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Read and display image
+            var fileName = args[0];
+            var fileStream = new FileStream(fileName, FileMode.Open);
+            Process.Start(fileName);
+
+            //Get face info for image
+            var faces = GetFaces(fileStream).GetAwaiter().GetResult();
+
+            //Print face info
+            FacesPrinter.Print(faces);
+        }
+
+        static async Task<Face[]> GetFaces(Stream fStream)
+        {
+            // Create Face API Client.
+            FaceServiceClient faceClient = new FaceServiceClient("748c9688f6af4638b4187b822976d7ed",
+                "https://westcentralus.api.cognitive.microsoft.com/face/v1.0");
+
+            var attrs = new List<FaceAttributeType>
+            {
+                FaceAttributeType.Age,
+                FaceAttributeType.Gender,
+                FaceAttributeType.Accessories,
+                FaceAttributeType.Hair,
+                FaceAttributeType.Emotion,
+                FaceAttributeType.FacialHair
+            };
+            return await faceClient.DetectAsync(fStream, returnFaceAttributes: attrs);
+        }
+    }
+}
